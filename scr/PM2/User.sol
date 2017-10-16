f1eaf02e2c09e4ab577155dee9dfa5893901c8e4
Mike <vinertsev@gmail.com>
2017-10-16 10:58:14 +0700

    Create User.sol

---
+++ scr/PM2/User.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.4.15;
+
+import '/src/common/ownership/Ownable.sol';
+import '/src/common/ownership/Ownable.sol';
+import '/src/PM2/Application.sol';
+
+contract User is Application, Ownable, SafeMath{
+	event RegistrationUser(address indexed user, uint info);	
+	struct _User {
+		uint8 status;
+		bool confirmation;
+		uint info;
+	}
+	
+	struct Purchase {
+		bool confirmation;
+	}
+	uint256 public commission;
+	mapping (address => uint256) public developerRevenue;
+	mapping (address => _User) public users;
+	mapping (address => mapping (uint =>  mapping (uint => Purchase))) public purchases;
+	function registrationUser (uint _info) public {
+		users[msg.sender] = _User({
+			status: 1,
+			confirmation: false,
+			info: _info
+		});
+		RegistrationUser(msg.sender,_info);	
+	}
+	
+	function buyApp (uint _idApp, uint _category) public payable {
+		assert(applications[_category][_idApp].value == msg.value);
+		//assert(users[_user].status>0 && users[_user].status<=1);
+		purchases[msg.sender][_category][_idApp].confirmation = true;
+		uint sum = sub(msg.value,div(msg.value,10));
+		developerRevenue[applications[_category][_idApp].developer] = add(developerRevenue[applications[_category][_idApp].developer],sum);
+		commission = add(commission,sub(msg.value,sum));
+	}
+}
