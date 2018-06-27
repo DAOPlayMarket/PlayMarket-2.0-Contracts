d9a14881fd19959b71cd640187f6068d16408a7f
Mike <vinertsev@gmail.com>
2018-06-27 12:42:58 +0700

    New version

---
+++ contracts/exchangeRate.sol
@@ -0,0 +1,67 @@
+pragma solidity ^0.4.21;
+
+/**
+ * @title Ownable contract - base contract with an owner
+ */
+contract Ownable {
+  
+  address public owner;
+  address public newOwner;
+
+  event OwnershipTransferred(address indexed _from, address indexed _to);
+  
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    assert(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address _newOwner) public onlyOwner {
+    assert(_newOwner != address(0));      
+    newOwner = _newOwner;
+  }
+
+  /**
+   * @dev Accept transferOwnership.
+   */
+  function acceptOwnership() public {
+    if (msg.sender == newOwner) {
+      emit OwnershipTransferred(owner, newOwner);
+      owner = newOwner;
+    }
+  }
+}
+
+/**
+ * @title exchangeRate
+ * @dev 
+ */
+contract exchangeRateContract is Ownable{
+  
+  uint public exchangeRate;
+  
+  function exchangeRateContract(uint _exchangeRate) public{
+    exchangeRate = _exchangeRate;
+  }
+  
+  /**
+   * @dev 
+   * @param _exchangeRate exchangeRate
+   */
+  function setExchangeRate(uint _exchangeRate) public onlyOwner {
+    exchangeRate = _exchangeRate;
+  }
+}
