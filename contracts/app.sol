e0fe1e78b9d6d619c8e399d7bf5849b750c9f808
Mike <vinertsev@gmail.com>
2018-07-06 15:19:00 +0700

    add changePrice, fix, new version solidity

--- contracts/app.sol
+++ contracts/app.sol
@@ -1,29 +1,29 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.21;
 
- /**
+/**
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
  */
 contract SafeMath {
 
-  function sub(uint256 x, uint256 y) internal pure returns (uint256) {
+  function safeSub(uint256 x, uint256 y) internal pure returns (uint256) {
     uint256 z = x - y;
     assert(z <= x);
     return z;
   }
 
-  function add(uint256 x, uint256 y) internal pure returns (uint256) {
+  function safeAdd(uint256 x, uint256 y) internal pure returns (uint256) {
     uint256 z = x + y;
     assert(z >= x);
     return z;
   }
 	
-  function div(uint256 x, uint256 y) internal pure returns (uint256) {
+  function safeDiv(uint256 x, uint256 y) internal pure returns (uint256) {
     uint256 z = x / y;
     return z;
   }
 	
-  function mul(uint256 x, uint256 y) internal pure returns (uint256) {
+  function safeMul(uint256 x, uint256 y) internal pure returns (uint256) {
     uint256 z = x * y;
     assert(x == 0 || z / x == y);
     return z;
@@ -81,7 +81,7 @@ contract Ownable {
    */
   function acceptOwnership() public {
     if (msg.sender == newOwner) {
-      OwnershipTransferred(owner, newOwner);
+      emit OwnershipTransferred(owner, newOwner);
       owner = newOwner;
     }
   }
@@ -117,12 +117,11 @@ contract Application is Agent, SafeMath {
     address developer;
     string hash;
     string hashTag;
-    uint256 value;
+    uint256 price;
     bool publish;
   }
 
   struct _ApplicationICO {
-    address adr;
     string hash;
     string hashTag;
   }
@@ -132,66 +131,146 @@ contract Application is Agent, SafeMath {
   mapping (address => mapping (uint =>  bool)) public purchases;
   mapping (address => uint256) public developerRevenue;
   
-  function registrationApplication(string _hash, string _hashTag, bool _publish, uint256 _value, address _dev) public onlyAgent returns (uint256) {
+  /**
+   * @dev 
+   * @param _hash hash
+   * @param _hashTag hashTag
+   * @param _publish publish
+   * @param _price price App
+   * @param _dev Developer address
+   * @return number of current applications
+   */
+  function registrationApplication(string _hash, string _hashTag, bool _publish, uint256 _price, address _dev) public onlyAgent returns (uint256) {
     applications.push(_Application({
       developer: _dev,
       hash: _hash,
       hashTag: _hashTag,
-      value: _value,
+      price: _price,
       publish: _publish
     }));
     return applications.length-1;
   }
   
-  function registrationApplicationICO (uint _idApp, address _addr, string _hash, string _hashTag, address _dev) public onlyAgent {
+  /**
+   * @dev 
+   * @param _idApp ID application
+   * @param _hash hash
+   * @param _hashTag hashTag
+   * @param _dev Developer address
+   */
+  function registrationApplicationICO(uint _idApp, string _hash, string _hashTag, address _dev) public onlyAgent {
     require(checkDeveloper(_idApp,_dev));
-    applicationsICO[_idApp].adr =_addr;
     applicationsICO[_idApp].hash =_hash;
     applicationsICO[_idApp].hashTag =_hashTag;
   }
 	
-  function changeHash (uint _idApp,  string _hash, string _hashTag, address _dev) public onlyAgent {
+   /**
+   * @dev 
+   * @param _idApp ID application
+   * @param _hash hash
+   * @param _hashTag hashTag
+   * @param _dev Developer address
+   */
+  function changeHash(uint _idApp,  string _hash, string _hashTag, address _dev) public onlyAgent {
     require(checkDeveloper(_idApp,_dev));
     applications[_idApp].hash =_hash;
     applications[_idApp].hashTag =_hashTag;
   }
   
-  function changePublish (uint _idApp, bool _publish, address _dev) public onlyAgent {
+  /**
+   * @dev 
+   * @param _idApp ID application
+   * @param _publish publish
+   * @param _dev Developer address
+   */
+  function changePublish(uint _idApp, bool _publish, address _dev) public onlyAgent {
     require(checkDeveloper(_idApp,_dev));
     applications[_idApp].publish =_publish;
   }
   
-  function changeIcoInfo (uint _idApp, address _addr, string _hash, string _hashTag, address _dev) public onlyAgent {
+  /**
+   * @dev 
+   * @param _idApp ID application
+   * @param _price new price application
+   * @param _dev Developer address
+   */
+  function changePrice(uint _idApp, uint256 _price, address _dev) public onlyAgent {
     require(checkDeveloper(_idApp,_dev));
-    applicationsICO[_idApp].adr =_addr;
+    applications[_idApp].price =_price;
+  }
+  
+  /**
+   * @dev 
+   * @param _idApp ID application
+   * @param _hash hash
+   * @param _hashTag hashTag
+   * @param _dev Developer address
+   */
+  function changeIcoHash(uint _idApp, string _hash, string _hashTag, address _dev) public onlyAgent {
+    require(checkDeveloper(_idApp,_dev));
     applicationsICO[_idApp].hash =_hash;
     applicationsICO[_idApp].hashTag =_hashTag;
   }
-  
+
+  /**
+   * @dev 
+   * @param _idApp ID application
+   * @param _dev Developer address
+   * @return boolean
+   */  
   function checkDeveloper(uint _idApp, address _dev) private constant returns (bool success) {
       require(applications[_idApp].developer == _dev);
       return true;
   }
-  
+
+  /**
+   * @dev 
+   * @param _idApp ID application
+   * @return developer address
+   */  
   function getDeveloper(uint _idApp) public onlyAgent constant  returns (address) {
     return applications[_idApp].developer;
   }
-  
-  function checkSum(uint _idApp, uint256 _value) private constant returns (bool success) {
-      require(applications[_idApp].value == _value);
+
+  /**
+   * @dev 
+   * @param _idApp ID application
+   * @param _price price application
+   * @return boolean
+   */  
+  function checkSum(uint _idApp, uint256 _price) private constant returns (bool success) {
+      require(applications[_idApp].price == _price);
       return true;
   }
 
-  function buyApp (uint _idApp, address _user, address _dev, uint _value, uint _proc) public onlyAgent {
-    require(checkSum(_idApp,_value));
+  /**
+   * @dev 
+   * @param _idApp ID application
+   * @param _user user address
+   * @param _dev Developer address
+   * @param _price price application
+   * @param _proc percentage payment to the developer
+   */  
+  function buyApp (uint _idApp, address _user, address _dev, uint _price, uint _proc) public onlyAgent {
+    require(checkSum(_idApp,_price));
     purchases[_user][_idApp] = true;
-    developerRevenue[_dev] = add(developerRevenue[_dev],div(mul(_value,_proc),100));
+    developerRevenue[_dev] = safeAdd(developerRevenue[_dev],safeDiv(safeMul(_price,_proc),100));
   }
 
+  /**
+   * @dev 
+   * @param _dev Developer address
+   */  
   function collectDeveloper(address _dev) public onlyAgent{
     developerRevenue[_dev] = 0;
   }
-  
+
+  /**
+   * @dev 
+   * @param _idApp ID application
+   * @param _user user address
+   * @return boolean
+   */    
   function checkBuy(uint _idApp, address _user) public constant returns (bool success) {
       return purchases[_user][_idApp];
   }
