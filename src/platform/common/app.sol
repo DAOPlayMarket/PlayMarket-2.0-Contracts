ee0b200d6c0d2028e2fe738c98955046e0992f30
Mike <vinertsev@gmail.com>
2018-09-19 20:21:23 +0700

    fix

--- src/platform/common/app.sol
+++ src/platform/common/app.sol
@@ -26,11 +26,9 @@ contract App is Agent, SafeMath {
     LogStorage = LogStorageI(_contract);    
   }  
 
- // add/register new application
-  function addApp(uint32 _hashType, uint32 _appType, uint _price, bool _publish, string _hash) external returns (uint) {
-    uint app = AppStorage.addApp(_hashType, _appType, store, _price, _publish, msg.sender, _hash);
+  function addApp(uint32 _hashType, uint32 _appType, uint _price, bool _publish, string _hash) external {
+    uint app = AppStorage.addApp(_hashType, _appType, _price, _publish, msg.sender, _hash);
     LogStorage.addAppEvent(app, _hashType, _appType, _price, _publish, msg.sender, _hash);
-    return app;
   }
   
   function setAppConfirmation(uint _app, bool _state) external onlyAgent {
@@ -39,7 +37,7 @@ contract App is Agent, SafeMath {
   }
   
   // after change hash application - confirmation sets to false
-  function changeHash(uint _app, string _hash, uint32 _hashType) external {  
+  function changeHashApp(uint _app, string _hash, uint32 _hashType) external {  
     require(AppStorage.getDeveloper(_app) == msg.sender);
     AppStorage.changeHashApp(_app, _hash, _hashType);
     LogStorage.changeHashAppEvent(_app, _hash, _hashType);
@@ -51,13 +49,38 @@ contract App is Agent, SafeMath {
     LogStorage.changePublishEvent(_app, _publish);
   }
 
-  function changePrice(uint _app, uint _price) external {
+  function setPrice(uint _app, uint _obj, uint _price) external {
     require(AppStorage.getDeveloper(_app) == msg.sender);
-    AppStorage.setPrice(_app, _price);
-    LogStorage.changePriceEvent(_app, _price);
+    AppStorage.setPrice(_app, _obj, _price);
+    LogStorage.setPriceEvent(_app, _obj, _price);
+  }
+
+  function setPrice(uint _app, uint[] _arrObj, uint[] _arrPrice) external {
+    require(AppStorage.getDeveloper(_app) == msg.sender);
+    AppStorage.setPrice(_app, _arrObj, _arrPrice);
   }
   
-  function changeHashICO(uint _app, string _hash, uint32 _hashType) external onlyAgent {
+  function setPrice(uint _app, uint[] _arrObj, uint _price) external {
+    require(AppStorage.getDeveloper(_app) == msg.sender);
+    AppStorage.setPrice(_app, _arrObj, _price);
+  }
+  
+  function setDuration(uint _app, uint _obj, uint _price) external {
+    require(AppStorage.getDeveloper(_app) == msg.sender);
+    AppStorage.setDuration(_app, _obj, _price);
+  }
+
+  function setDuration(uint _app, uint[] _arrObj, uint[] _arrPrice) external {
+    require(AppStorage.getDeveloper(_app) == msg.sender);
+    AppStorage.setDuration(_app, _arrObj, _arrPrice);
+  }
+  
+  function setDuration(uint _app, uint[] _arrObj, uint _price) external {
+    require(AppStorage.getDeveloper(_app) == msg.sender);
+    AppStorage.setDuration(_app, _arrObj, _price);
+  }
+  
+  function changeHashAppICO(uint _app, string _hash, uint32 _hashType) external onlyAgent {
     require(AppStorage.getDeveloper(_app) == msg.sender);
     AppStorage.changeHashAppICO(_app, _hash, _hashType);
     LogStorage.changeHashAppICOEvent(_app, _hash, _hashType);
@@ -67,13 +90,25 @@ contract App is Agent, SafeMath {
     return AppStorage.getDeveloper(_app);
   }
 
-  // check application buy
-  function checkBuy(uint _app, address _user) external view returns (bool success) {
-    return AppStorage.checkBuy(_app, _user, 0);
-  }
-
   // check objects buy
-  function checkBuy(uint _app, address _user, uint _obj) external view returns (bool success) {
-    return AppStorage.checkBuy(_app, _user, _obj);
+  function getBuyObject(uint _app, address _user, uint _obj) external view returns (bool success) {
+    return AppStorage.getBuyObject(_app, _user, _obj);
+  }
+  
+  // check objects buy
+  function getTimeSubscription(uint _app, address _user, uint _obj) external view returns (uint _endTime) {
+    return AppStorage.getTimeSubscription(_app, _user, _obj);
+  }
+  
+  /**
+   * @dev We do not store the data in the contract, but generate the event. This allows you to make feedback as cheap as possible. The event generation costs 8 wei for 1 byte, and data storage in the contract 20,000 wei for 32 bytes
+   * @param _app voice application identifier
+   * @param vote voter rating
+   * @param description voted opinion
+   * @param txIndex identifier for the answer
+   */
+  function feedbackRating(uint _app, uint vote, string description, bytes32 txIndex) external {
+    require( vote > 0 && vote <= 10);
+    LogStorage.feedbackRatingEvent(msg.sender, _app, vote, description, txIndex, block.timestamp);
   }
 }
