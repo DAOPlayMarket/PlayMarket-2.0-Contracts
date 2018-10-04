e1e5ac9f14fa2eadbb346d8d843f32a3f1758f0e
Mike <vinertsev@gmail.com>
2018-10-04 16:44:39 +0700

    fix

--- src/proxy.sol
+++ src/proxy.sol
@@ -10,51 +10,51 @@ contract Proxy is Agent {
         // version PM 2.0
         bytes32 version;
         //support end time (0 - indefinite)
-        uint256 timestamp;
+        uint256 endTime;
     }
     
     Version[] public versions;
     
-    event addVersionLog(address PlayMarket, address ICO, bytes32 version, uint256 timestamp, uint256 i);
-    event changeVersionLog(address PlayMarket, address ICO, bytes32 version, uint256 timestamp, uint256 i);
+    event addVersionLog(address PlayMarket, address ICO, bytes32 version, uint256 endTime, uint256 i);
+    event changeVersionLog(address PlayMarket, address ICO, bytes32 version, uint256 endTime, uint256 i);
     event lastVersionLog(bytes32 version, uint256 i);
     
-    constructor(address _PlayMarket, address _ICO, bytes32 _version, uint256 _timestamp) public {
+    constructor(address _PlayMarket, address _ICO, bytes32 _version, uint256 _endTime) public {
         require(_PlayMarket != address(0) && _ICO != address(0));
-        versions.push(Version( _PlayMarket, _ICO, _version, _timestamp));
-        emit addVersionLog(_PlayMarket, _ICO, _version, _timestamp, versions.length);
+        versions.push(Version( _PlayMarket, _ICO, _version, _endTime));
+        emit addVersionLog(_PlayMarket, _ICO, _version, _endTime, versions.length);
         emit lastVersionLog(_version, versions.length);
     }
 
-     function addVersion(address _PlayMarket, address _ICO, bytes32 _version, uint256 _timestamp) public onlyAgent {
+     function addVersion(address _PlayMarket, address _ICO, bytes32 _version, uint256 _endTime) public onlyAgent {
         require(_PlayMarket != address(0) && _ICO != address(0));
-        versions.push(Version( _PlayMarket, _ICO, _version, _timestamp));
-        emit addVersionLog(_PlayMarket, _ICO, _version, _timestamp, versions.length);
+        versions.push(Version( _PlayMarket, _ICO, _version, _endTime));
+        emit addVersionLog(_PlayMarket, _ICO, _version, _endTime, versions.length);
         emit lastVersionLog(_version, versions.length);
     }
 
-     function changeVersion(address _PlayMarket, address _ICO, bytes32 _version, uint256 _timestamp, uint256 _i) public onlyAgent {
+     function changeVersion(address _PlayMarket, address _ICO, bytes32 _version, uint256 _endTime, uint256 _i) public onlyAgent {
         require(_PlayMarket != address(0) && _ICO != address(0));
-        versions[_i] = (Version( _PlayMarket, _ICO, _version, _timestamp));
-        emit changeVersionLog(_PlayMarket, _ICO, _version, _timestamp, _i);
+        versions[_i] = (Version( _PlayMarket, _ICO, _version, _endTime));
+        emit changeVersionLog(_PlayMarket, _ICO, _version, _endTime, _i);
     }
     
-    function getLastVersion() public view returns(address, bytes32, uint256, uint256){
-        for(uint i = versions.length-1; i >0; i--) {
-            if(versions[i].timestamp == 0 || versions[i].timestamp> block.timestamp){
-                return (versions[i].PlayMarket, versions[i].version, versions[i].timestamp, i);
+    function getLastVersion() public view returns (address PlayMarket, address ICO, bytes32 version, uint256 endTime, uint256 number){
+        for(uint i = versions.length-1; i > 0; i--) {
+            if(versions[i].endTime == 0 || versions[i].endTime > block.timestamp){
+                return (versions[i].PlayMarket, versions[i].ICO, versions[i].version, versions[i].endTime, i);
             }
         }
-        return (versions[0].PlayMarket, versions[0].version, versions[0].timestamp, i);
+        return (versions[0].PlayMarket, versions[0].ICO, versions[0].version, versions[0].endTime, 0);
     }
     
-    function getVersion(bytes32 _version) public view returns(address, bytes32, uint256, uint256){
+    function getVersion(bytes32 _version) public view returns  (address PlayMarket, address ICO, bytes32 version, uint256 endTime, uint256 number){
         if(versions[0].version == _version) {
-             return (versions[0].PlayMarket, versions[0].version, versions[0].timestamp, i);
+             return (versions[0].PlayMarket, versions[0].ICO, versions[0].version, versions[0].endTime, 0);
         }
-        for(uint i = versions.length-1; i >0; i--) {
+        for(uint i = versions.length-1; i > 0; i--) {
             if(versions[i].version == _version) {
-                return (versions[i].PlayMarket, versions[i].version, versions[i].timestamp, i);
+                return (versions[i].PlayMarket, versions[i].ICO, versions[i].version, versions[i].endTime, i);
             }
         }
     }
